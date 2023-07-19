5344a9cf3c1b4e5fd187a6045ebecafa8f0fd83c
piyer2007 <129233218+piyer2007@users.noreply.github.com>
2023-07-19 09:15:27 -0400

    Add files via upload

--- findmissingnumbers.py
+++ findmissingnumbers.py
@@ -57,7 +57,7 @@ class Solution:
             num = 0;
             for i in range(len(numbers)-1):
                 num = numbers[i]
-                if num+1 != numbers[i+1] and num != numbers[i+1]:
+                if num+1 != numbers[i+1] or num != numbers[i+1]:
                     add = (int(num+1))
                     new = [add]
                     miss = miss + new
